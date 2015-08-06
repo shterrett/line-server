263138964b90e5f6adf6a8865923964f24ea4f36
Stuart Terrett <shterrett@gmail.com>
2015-08-05 21:39:48 -0400

    Changes from testing another machine

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
 source "http://rubygems.org"
 
-ruby '2.1.5'
-
 # Specify your gem's dependencies in synxis.gemspec
 gemspec
--- start.rb
+++ start.rb
@@ -1,3 +1,5 @@
+raise 'Must have ruby >= 2.1' if RUBY_VERSION.to_f < 2.1
+
 require_relative 'lib/line_server'
 
 path_to_original_file = ARGV[0]
@@ -7,6 +9,9 @@ Configuration.set do |config|
   config.hash_file_dir = './tmp'
 end
 
+if !Dir.exist?(Config.hash_file_dir)
+  Dir.mkdir(Config.hash_file_dir)
+end
 DirectoryCleaner.new(Configuration.get.hash_file_dir).clean
 
 FileHasher.new(file: path_to_original_file,
